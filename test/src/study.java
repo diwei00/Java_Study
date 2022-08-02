01997b562ca45639066825acc0581d53828b8d9a
diwei00 <2945608334@qq.com>
2022-08-02 18:36:42 +0800

    Java学习

---
+++ test/src/study.java
@@ -0,0 +1,11 @@
+public class study {
+    public static void main(String[] args) {
+        System.out.println("wuhao");
+        int a = 10;
+        for (int i = 0; i < 20; i++) {
+            a++;
+        }
+        System.out.println(a);
+
+    }
+}
