ca89d5f5ce78a6770c47e9e1a453ba07e27c061b
diwei00 <2945608334@qq.com>
2022-08-12 21:52:16 +0800

    封装的学习

---
+++ Test8.java
@@ -0,0 +1,22 @@
+public class Test8 {
+
+    private  int count;
+
+    public static void main(String[] args) {
+
+        Test8 test = new Test8(88);
+        test.count = 20;
+
+
+        System.out.println(test.count);
+        System.out.println(test.count);
+    }
+
+    Test8(int a) {
+
+        count = a;
+
+    }
+
+
+}
